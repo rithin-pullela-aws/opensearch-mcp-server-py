b63967901560ae2c6741793c03fccfa89e78b7ae
rithin-pullela-aws <rithinp@amazon.com>
2025-11-14 16:14:34 -0800

    Revert "Fixing ListIndexTool when listing all indices (#133)"
    This reverts commit 00f8cdbbcf17c8a14b557a735ec2731899272592.
    
    Signed-off-by: rithin-pullela-aws <rithinp@amazon.com>

--- src/opensearch/helper.py
+++ src/opensearch/helper.py
@@ -16,7 +16,7 @@ async def list_indices(args: ListIndicesArgs) -> json:
     from .client import get_opensearch_client
 
     async with get_opensearch_client(args) as client:
-        response = await client.cat.indices()
+        response = await client.cat.indices(format='json')
         return response
 
 
